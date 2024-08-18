cda0d12a568cb74e24078a0eef68130c1d49d926
Anton Doronin <doronantonin@gmail.com>
2024-08-18 16:51:58 +0500

    simple redis caching added

--- url-shortener/url-shortener-backend/src/main/java/ru/doronin/shortener/config/ApplicationConfig.java
+++ url-shortener/url-shortener-backend/src/main/java/ru/doronin/shortener/config/ApplicationConfig.java
@@ -1,14 +1,30 @@
 package ru.doronin.shortener.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import ru.doronin.Snowflake;
 import ru.doronin.shortener.config.properties.ApplicationProperties;
+import ru.doronin.shortener.domain.model.PageLink;
+
+import java.time.Duration;
+
+import static com.fasterxml.jackson.core.JsonGenerator.Feature.IGNORE_UNKNOWN;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 @Configuration
+@EnableCaching
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "ru.doronin.shortener.domain.repository")
 @RequiredArgsConstructor
@@ -18,5 +34,32 @@ public class ApplicationConfig {
     @Bean
     public Snowflake snowflakeGenerator() {
         return new Snowflake(applicationProperties.getNodeId());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .enable(IGNORE_UNKNOWN)
+                .disable(FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer cacheManagerBuilderCustomizer() {
+        return builder -> builder
+                .withCacheConfiguration("longLinksCache",
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .disableCachingNullValues()
+                                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(objectMapper(), PageLink.class)))
+                                .entryTtl(Duration.ofHours(1)))
+                .withCacheConfiguration("shortLinksCache",
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .disableCachingNullValues()
+                                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(objectMapper(), PageLink.class)))
+                                .entryTtl(Duration.ofMinutes(5)));
     }
 }
--- url-shortener/url-shortener-backend/src/main/java/ru/doronin/shortener/domain/service/PageLinkService.java
+++ url-shortener/url-shortener-backend/src/main/java/ru/doronin/shortener/domain/service/PageLinkService.java
@@ -1,6 +1,7 @@
 package ru.doronin.shortener.domain.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 import ru.doronin.Snowflake;
 import ru.doronin.shortener.domain.model.PageLink;
@@ -12,28 +13,62 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PageLinkService {
+    private static final String SHORT_LINKS_CACHE_NAME = "shortLinksCache";
+    private static final String LONG_LINKS_CACHE_NAME = "longLinksCache";
     private final PageLinkRepository pageLinkRepository;
     private final Snowflake snowflakeGenerator;
+    private final CacheManager cacheManager;
 
     public PageLink createOrGetShortLink(String longLink) {
-        return pageLinkRepository.findPageLinkByLongUrl(longLink)
+        return getFromCacheByLongLink(longLink)
                 .orElseGet(() -> {
-                    var id = snowflakeGenerator.nextId();
-                    var shortLink = PageLink.builder()
-                            .id(id)
-                            .shortUrl(Base62.encode(id))
-                            .longUrl(longLink)
-                            .build();
-                    return pageLinkRepository.save(shortLink);
+                    var pageLink = pageLinkRepository.findPageLinkByLongUrl(longLink)
+                            .orElseGet(() -> {
+                                var id = snowflakeGenerator.nextId();
+                                var shortLink = PageLink.builder()
+                                        .id(id)
+                                        .shortUrl(Base62.encode(id))
+                                        .longUrl(longLink)
+                                        .build();
+                                return pageLinkRepository.save(shortLink);
+                            });
+                    updateCaches(pageLink);
+                    return pageLink;
                 });
+    }
+
+    private void updateCaches(PageLink pageLink) {
+        Optional.ofNullable(cacheManager.getCache(SHORT_LINKS_CACHE_NAME))
+                .ifPresent(cache -> cache.put(pageLink.getShortUrl(), pageLink));
+        Optional.ofNullable(cacheManager.getCache(LONG_LINKS_CACHE_NAME))
+                .ifPresent(cache -> cache.put(pageLink.getLongUrl(), pageLink));
+    }
+
+    private Optional<PageLink> getFromCacheByLongLink(String longLink) {
+        return getFromCache(longLink, LONG_LINKS_CACHE_NAME);
+    }
+
+    private Optional<PageLink> getFromCacheByShortLink(String shortLink) {
+        return getFromCache(shortLink, SHORT_LINKS_CACHE_NAME);
+    }
+
+    private Optional<PageLink> getFromCache(String key, String cacheName) {
+        var cache = cacheManager.getCache(cacheName);
+        return (cache != null) ? Optional.ofNullable(cache.get(key, PageLink.class)) : Optional.empty();
     }
 
     public Optional<String> getLongLinkByShort(String shortLink) {
         var linkId = Base62.decode(shortLink);
-        return pageLinkRepository.findById(linkId)
-                .map(link -> {
+        return getFromCacheByShortLink(shortLink)
+                .or(() -> pageLinkRepository.findById(linkId)
+                        .map(link -> {
+                            updateCaches(link);
+                            return link;
+                        })
+                )
+                .map(pageLink -> {
                     pageLinkRepository.findAndIncrementPageLinkById(linkId);
-                    return link.getLongUrl();
+                    return pageLink.getLongUrl();
                 });
     }
 }
